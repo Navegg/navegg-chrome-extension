f6131be48f39735cf9cc605cfb02ae59e40b6bc6
William Marquardt <marquardt@navegg.com>
2017-11-23 11:44:51 -0200

    Fix variable names conflicts

--- tag_getter.js
+++ tag_getter.js
@@ -1,5 +1,20 @@
-var scr = document.createElement("script");
-scr.type="text/javascript";
-fn = "var have_tag = false; for(k in window){ if( k.match('nvg[1-9]')){ have_tag = true;}}; var dv = document.createElement('div'); dv.id='nvg_have'; dv.style.display='none'; dv.innerHTML = have_tag; document.body.appendChild(dv);"
-scr.innerHTML = fn;
-document.body.appendChild(scr)
+/*
+*  This js is injected into the pages
+*  So the name of all variables mus be UNIQUE
+*  Because generic names maybe have 
+*  conflicts with site variable names
+*/
+var navegg_scr_element = document.createElement("script");
+navegg_scr_element.type="text/javascript";
+fn = "var have_navegg_tag = false; \
+        for(var navegg_window_loop in window){ \
+            if( navegg_window_loop.match('nvg[1-9]')){ \
+            have_navegg_tag = true;} \
+        }; \
+        var navegg_inject_dv = document.createElement('div'); \
+        navegg_inject_dv.id='nvg_have'; \
+        navegg_inject_dv.style.display='none'; \
+        navegg_inject_dv.innerHTML = have_navegg_tag; \
+        document.body.appendChild(navegg_inject_dv);"
+navegg_scr_element.innerHTML = fn;
+document.body.appendChild(navegg_scr_element);
